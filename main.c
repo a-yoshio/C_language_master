f2a3383232c2122ca13a45597e8f91bb4df4df7e
a_yoshino <atsuko.yoshino@rakus.co.jp>
2020-10-25 10:08:14 +0900

    try interpriter

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(void){
     printf("Hello World!\n");
+    printf("Which number do you like?");
+    // try interpriter
+    int number;
+    scanf("%d", &number);
+    printf("OK! I understand! You like number is -> %d", number);
     return 0;
 }
