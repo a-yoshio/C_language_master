fb97ac276175f49376bfd81f210032fd66c82696
a_yoshino <atsuko.yoshino@rakus.co.jp>
2020-10-25 11:09:21 +0900

    try cast and arithmetic operations

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 int main(void){
     printf("Hello World!\n");
-    printf("Which number do you like?");
+    // try alert
+    printf("Let's alert!! \a\a\a\n");
     // try interpriter
+    printf("Which number do you like?");
     int number;
     scanf("%d", &number);
-    printf("OK! I understand! You like number is -> %d", number);
+    printf("OK! I understand! You like number is -> %d ¥n", number);
     // put function
     printf("New Line!|");
     printf("Not new line...|");
     puts("Let's new line!|");
     printf("New Line!|");
+    // basic arithmetic operations
+    puts("Please enter appropriate number of two.");
+    int vx = 3;
+    int vy = 1;
+    printf("vx + vy = %d¥\n", vx + vy);
+    printf("vx + vy = %d¥\n", vx + vy);
+    printf("vx / vy = %d¥\n", vx / vy);
+    printf("vx %% vy = %d¥\n", vx % vy);
+    // cast
+    vx = 5;
+    vy = 2;
+    printf("cast: vx / vy = %f¥\n", (double) vx / vy);
+    printf("cast: vx / vy = %.1f¥\n", (double) vx / vy);
+    printf("cast: vx / vy = %5.1f¥\n", (double) vx / vy);
+    printf("cast: vx / vy = %05.1f¥\n", (double) vx / vy);
     return 0;
 }
